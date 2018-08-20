7f503911b7e9696aa498f63ba1ebbcd68870af74
qqyonghui <qqyonghui@163.com>
2018-08-20 20:13:37 +0800

    新增功能：增加登录页

--- HMS.Web/HMS.Web/Startup.cs
+++ HMS.Web/HMS.Web/Startup.cs
@@ -59,7 +59,7 @@ namespace HMS.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
